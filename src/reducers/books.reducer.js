061f064dac6d9d143bd8cb4df9bf96498540c81d
Danilo Aleixo <danilo_aleixo@hotmail.com>
2018-03-04 19:06:50 -0300

    Fim da seção 4
    - Coloca outros reducers e faz a app funcionar

--- src/reducers/books.reducer.js
+++ src/reducers/books.reducer.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'Javascript: The Good Parts' },
-		{ title: 'Harry Potter 1' },
-		{ title: 'Harry Potter 2' },
-		{ title: 'Harry Potter 4' },
+		{ title: 'Javascript: The ,Good Parts', pages: 100 },
+		{ title: 'Harry Potter 1', pages: 200 },
+		{ title: 'Harry Potter 2', pages: 300 },
+		{ title: 'Harry Potter 4', pages: 400 },
 	]
 }
